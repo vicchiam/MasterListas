53acdde5c071408aba69179b9637c504a0f46f69
vca <vca@vca.org>
2017-11-05 15:01:43 +0100

    Imagen que se colapsa

--- app/src/main/java/org/example/masterlistas/ListasActivity.java
+++ app/src/main/java/org/example/masterlistas/ListasActivity.java
@@ -1,7 +1,10 @@
 package org.example.masterlistas;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListasActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class ListasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listas);
+
+        FloatingActionButton fab=(FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG) .show();
+            }
+
+        });
+
     }
 }
