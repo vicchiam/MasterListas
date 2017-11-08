3090b09e48175d2c6f667d2fa3f366291fa8fa06
vca <vca@vca.org>
2017-11-09 00:39:15 +0100

    Mostrar ocultar contrasenya

--- app/src/main/java/org/example/masterlistas/InicioSesionActivity.java
+++ app/src/main/java/org/example/masterlistas/InicioSesionActivity.java
@@ -2,6 +2,11 @@ package org.example.masterlistas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class InicioSesionActivity extends AppCompatActivity {
 
@@ -10,4 +15,22 @@ public class InicioSesionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
     }
+
+    public void loguearCheckbox(View v) {
+        CheckBox recordarme= (CheckBox) findViewById(R.id.recordarme);
+        String s = "Recordar datos de usuario: " + (recordarme.isChecked() ? "Sí" : "No");
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
+    public void mostrarContraseña(View v) {
+        EditText contraseña = (EditText) findViewById(R.id.contraseña);
+        CheckBox mostrar = (CheckBox) findViewById(R.id.mostrar_contraseña);
+        if (mostrar.isChecked()) {
+            contraseña.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+        }
+        else {
+            contraseña.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        }
+    }
+
 }
