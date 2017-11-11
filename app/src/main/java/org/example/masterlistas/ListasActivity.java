e8923ca2adf66aed2fc35c5452b5576cf6d2f179
vca <vca@vca.org>
2017-11-11 12:40:07 +0100

    Navigation Drawer Elastico

--- app/src/main/java/org/example/masterlistas/ListasActivity.java
+++ app/src/main/java/org/example/masterlistas/ListasActivity.java
@@ -15,25 +15,34 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
+import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListasActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ListasActivity extends AppCompatActivity /*implements NavigationView.OnNavigationItemSelectedListener*/ {
+
+    private FlowingDrawer mDrawer;
 
     private RecyclerView recycler;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager lManager;
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listas);
 
-        FloatingActionButton fab=(FloatingActionButton) findViewById(R.id.fab);
+        fab=(FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
                 Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG) .show();
@@ -70,9 +79,11 @@ public class ListasActivity extends AppCompatActivity implements NavigationView.
                         Intent intent = new Intent(ListasActivity.this, DetalleListaActivity.class);
                         intent.putExtra("numeroLista", position);
                         //startActivity(intent);
-                        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
-                                ListasActivity.this,
-                                new Pair<View, String>(v.findViewById(R.id.imagen),getString(R.string.transition_name_img))
+
+                        ActivityOptionsCompat options = ActivityOptionsCompat.
+                                makeSceneTransitionAnimation(ListasActivity.this,
+                                        new Pair<View, String>(v.findViewById(R.id.imagen),getString(R.string.transition_name_img)),
+                                        new Pair<View, String>(fab,getString(R.string.transition_name_boton))
                         );
                         ActivityCompat.startActivity(ListasActivity.this, intent, options .toBundle());
                     }
@@ -83,15 +94,40 @@ public class ListasActivity extends AppCompatActivity implements NavigationView.
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         // Navigation Drawer
+        /*
         DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string. drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById( R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        */
+
+        // Navigation Drawer
+        NavigationView navigationView = (NavigationView) findViewById( R.id.vNavigation);
+        navigationView.setNavigationItemSelectedListener( new NavigationView.OnNavigationItemSelectedListener() {
+            @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Toast.makeText(getApplicationContext(),menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        mDrawer = (FlowingDrawer) findViewById(R.id.drawerlayout);
+        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mDrawer.toggleMenu();
+            }
+        });
+
+        Transition lista_enter = TransitionInflater.from(this) .inflateTransition(R.transition.transition_lista_enter);
+        getWindow().setEnterTransition(lista_enter);
+
     }
 
+    /*
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -109,15 +145,23 @@ public class ListasActivity extends AppCompatActivity implements NavigationView.
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    */
 
     @Override public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout);
+        /*DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         }
         else {
             super.onBackPressed();
+        }*/
+        if (mDrawer.isMenuVisible()) {
+            mDrawer.closeMenu();
+        }
+        else {
+            super.onBackPressed();
         }
     }
+
 
 }
